2f9d692b683d6ab744c4b0db96425c65580b96f1
Hrishit-Gadia <80762914+Hrishit-Gadia@users.noreply.github.com>
2021-08-18 12:31:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
-function preload(){
+function preload() {
 
 }
 
-function setup(){
-   Canvas = createCanvas(300,300);
-   Canvas.center(); 
+function setup() {
+    Canvas = createCanvas(300, 300);
+    Canvas.center();
+    Preview = createCapture(VIDEO);
+    Preview.size(300, 300)
+    Preview.hide();
+    status = ml5.poseNet(Preview, Report);
+    status.on('pose', Movenment);
 }
 
-function draw(){
-    image(Canvas,0,0,300,300);
+function draw() {
+    image(Preview, 0, 0, 300, 300);
 }
+
+function Report(){
+    console.log("Fine!");
+}
+
+function Movenment(Result) {
+    if(Result > 0) {
+        console.log(Result);
+        console.log("X = "+Result[0].pose.nose.x);
+        console.log("Y = "+Result[0].pose.nose.y);
+    }}
