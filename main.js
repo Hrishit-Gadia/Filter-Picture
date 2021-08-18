396fbb69b718d0ee81048059119a42c07623907f
Hrishit-Gadia <80762914+Hrishit-Gadia@users.noreply.github.com>
2021-08-18 21:55:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function preload() {
-
+    Female = '';
+    Male = 'https://i.postimg.cc/D02zpZct/Male.jpg';
 }
 
 function setup() {
@@ -8,21 +9,24 @@ function setup() {
     Preview = createCapture(VIDEO);
     Preview.size(300, 300)
     Preview.hide();
-    status = ml5.poseNet(Preview, Report);
-    status.on('pose', Movenment);
+    Status = ml5.poseNet(Preview, Report);
+    Status.on('pose', Movenment);
 }
 
 function draw() {
     image(Preview, 0, 0, 300, 300);
 }
 
-function Report(){
+function Report() {
     console.log("Fine!");
 }
 
 function Movenment(Result) {
-    if(Result > 0) {
+    if (Result.length > 0) {
         console.log(Result);
-        console.log("X = "+Result[0].pose.nose.x);
-        console.log("Y = "+Result[0].pose.nose.y);
-    }}
+        console.log("X = " + Result[0].pose.nose.x);
+        console.log("Y = " + Result[0].pose.nose.y);
+        Xs = Result[0].pose.nose.x;
+        Ys = Result[0].pose.nose.y;
+    }
+}
